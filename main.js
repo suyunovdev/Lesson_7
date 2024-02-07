148eacd556299994d3ef40837a0b27d6786b31a1
suyunovdev <153995037+suyunovdev@users.noreply.github.com>
2024-02-07 22:06:36 +0500

    main

---
+++ main.js
@@ -0,0 +1,12 @@
+//uyga vazifa////////////////
+//1-masala
+// function kattaHarflar(n) {
+//     const alfavit = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z';
+//     const kattaHarflar = alfavit.slice(0, n);
+//     return kattaHarflar;
+// }
+// const n = +prompt(`topmoqchi bo'lgan harfni raqamini kiriting`);
+// console.log(kattaHarflar(n));
+
+//2-masala
+
